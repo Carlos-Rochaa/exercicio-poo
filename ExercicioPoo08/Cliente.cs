7158d8855a69b0899bcc14696a9b28f686d4a50e
Carlos Rocha <carlosrocha1896@gmail.com>
2023-11-26 23:13:36 -0300

    first commit

---
+++ ExercicioPoo08/Cliente.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using System.Globalization; 
+
+namespace ExercicioPoo08
+{
+    internal class Cliente
+    {
+        public string Name {  get; set; } 
+        public int Conta { get; private set; }
+        public double Saldo { get; private set; } 
+
+        public Cliente()
+        {
+
+        }
+
+        public Cliente(string name, int conta, double depositoInicial) : this(name, conta)
+        {
+
+            Deposito(depositoInicial); 
+        }
+
+        public Cliente(string name, int conta)
+        {
+            Name = name;
+            Conta = conta;
+            Saldo = 0; 
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor; 
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= valor + 5; //taxa de 5$ para o saque. 
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Dados da conta: ");
+            Console.Write("Conta " + Conta + ", Titular: " + Name + ", Saldo: $ " + Saldo.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        public void PrintInfoUpdated()
+        {
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine("Conta " + Conta + ", Titular: " + Name + ", Saldo: $ " + Saldo.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ ExercicioPoo08/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization; 
+namespace ExercicioPoo08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente; 
+            Console.Write("Entre com o número da conta: "); 
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o nome do titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? (s/n): ");
+            string resposta = Console.ReadLine();
+            if (resposta == "s" || resposta == "S")
+            {
+                Console.Write("Entre o valor do depósito inicial: "); 
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                 cliente = new Cliente(nomeTitular, numeroConta, depositoInicial);
+            }
+            else
+            {
+                 cliente = new Cliente(nomeTitular, numeroConta); 
+            }
+             
+
+            cliente.PrintInfo();
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para depósito: "); 
+            double valorDeposito = double.Parse(Console.ReadLine() , CultureInfo.InvariantCulture);
+            cliente.Deposito(valorDeposito);
+            Console.WriteLine(); 
+            cliente.PrintInfoUpdated();
+
+            Console.Write("Entre um valor para saque: ");
+            double valorSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            cliente.Saque(valorSaque);
+            Console.WriteLine();
+            cliente.PrintInfoUpdated();
+
+
+
+        }
+    }
+}
